69ca4495edc3ce63736e3642001c24becfa94715
86188 <1742413455@qq.com>
2021-04-30 22:57:04 +0800

    Test

--- src/practice/Test.java
+++ src/practice/Test.java
@@ -1,12 +1,10 @@
 package practice;
+public abstract class Test{
 
-public abstract class Test {
-
-    }
+}
     class Test1 {
         public static void main(String[] args) {
             new Test();//error 如下图
         }
     }
-
 
